a4ae14adb2f1d43333f184393e91b61fed906e6a
Mehmet Nazliay <65411256+mhmtnzly@users.noreply.github.com>
2023-06-07 13:10:11 +0000

    update

--- Lesson6-Sorting/NumberOfDiscIntersections/main.go
+++ Lesson6-Sorting/NumberOfDiscIntersections/main.go
@@ -1,5 +1,6 @@
 package solution
-
+// Complexity is high ????????????????????????
+// O(N*log(N)) or O(N) or O(N**2)
 // you can also use imports, for example:
 // import "fmt"
 // import "sort"
@@ -9,21 +10,17 @@ package solution
 // fmt.Println("this is a debug message")
 
 func Solution(A []int) int {
-    // sort.Ints(A)\
-    // length := len(A)
     count := 0
     for index, value := range A{
-        // fmt.Println(index-value, index+value)
         for i := index-1; i>=0; i--{
             if index-value<=i-A[i] || A[i]+i>=index-value{
                 count++
-                // fmt.Println(i-A[i],index-value, value, index, i)
+                if count > 10000000{
+                    return -1
             }
-            if count > 10000000{
-            return -1
+            }
+            
         }
-        }
-        
     }
     return count
 }
